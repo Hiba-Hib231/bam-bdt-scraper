009e5f848c44064e1230cfd71d1fd74fc011bbcf
Hiba-Hib231 <aitaddihiba7@gmail.com>
2025-07-31 12:40:29 +0100

    Update setup.py
    Ajout du fichier setup.py pour installation via pip

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(
     name="bam-bdt-scraper",
     version="0.1",
-    py_modules=["bam_bdt_downloader"],  # remplace par le nom de ton fichier .py sans l'extension
+    py_modules=["bam_bdt_downloader"],  # nom de ton fichier Python sans .py
     install_requires=[
         "selenium",
+        "webdriver_manager",
     ],
     author="Hiba Hib",
     description="Un scraper pour BAM BDT",
